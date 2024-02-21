500e2761bda089e09780924c71a5a9f627577b68
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-21 19:55:42 -0400

    HC - add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavHeaderComponent],
+  imports: [RouterOutlet, RouterLink, NavHeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
