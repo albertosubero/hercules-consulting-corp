e4f4f444daf976eca29fe84a5cdfe03e6ec91220
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-22 19:41:23 -0400

    HC: add training view #section-00

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { ServicesViewComponent } from './views/services-view/services-view.component';
+import { TrainingViewComponent } from './views/training-view/training-view.component';
 
 export const routes: Routes = [
   { path: '', title:"Home Page", component: HomeViewComponent },
   { path: 'services', title:"Services Page", component: ServicesViewComponent },
+  { path: 'training', title: "Training Page", component: TrainingViewComponent }
 ];
---
+++ src/app/views/training-view/training-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './training-view.component.html',
+  styleUrl: './training-view.component.css'
+})
+export class TrainingViewComponent {
+
+}
