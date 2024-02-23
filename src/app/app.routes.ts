d942b1c29a24df54da7c98f280ea32854f44b519
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-23 14:45:01 -0400

    HC: add about-us view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeViewComponent } from './views/home-view/home-view.component';
 import { ServicesViewComponent } from './views/services-view/services-view.component';
 import { TrainingViewComponent } from './views/training-view/training-view.component';
 import { RecruitmentComponent } from './views/recruitment/recruitment.component';
+import { AboutUsComponent } from './views/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', title:"Home Page", component: HomeViewComponent },
   { path: 'services', title:"Services Page", component: ServicesViewComponent },
   { path: 'training', title: "Training Page", component: TrainingViewComponent },
-  { path: 'recruitment', title: "Recruitment Page", component: RecruitmentComponent }
+  { path: 'recruitment', title: "Recruitment Page", component: RecruitmentComponent },
+  { path: 'about-us', title: "Careers Page", component: AboutUsComponent }
 ];
