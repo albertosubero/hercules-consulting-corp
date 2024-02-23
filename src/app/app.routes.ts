6f528f900f78ba975d88e12e3032b4201cd43f6c
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-23 19:59:25 -0400

    HC: add contact view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { TrainingViewComponent } from './views/training-view/training-view.compo
 import { RecruitmentComponent } from './views/recruitment/recruitment.component';
 import { AboutUsComponent } from './views/about-us/about-us.component';
 import { CareersComponent } from './views/careers/careers.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', title:"Home Page", component: HomeViewComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'training', title: "Training Page", component: TrainingViewComponent },
   { path: 'recruitment', title: "Recruitment Page", component: RecruitmentComponent },
   { path: 'about-us', title: "About us Page", component: AboutUsComponent },
-  { path: 'careers', title: "Careers Page", component: CareersComponent }
+  { path: 'careers', title: "Careers Page", component: CareersComponent },
+  { path: 'contact', title: "Contact Page", component: ContactComponent }
 ];
