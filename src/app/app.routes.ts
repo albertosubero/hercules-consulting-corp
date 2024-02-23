fe9faafb00581ad9669e30ded6aa7051c8c442a2
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-23 19:55:11 -0400

    HC: finish careers view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ServicesViewComponent } from './views/services-view/services-view.compo
 import { TrainingViewComponent } from './views/training-view/training-view.component';
 import { RecruitmentComponent } from './views/recruitment/recruitment.component';
 import { AboutUsComponent } from './views/about-us/about-us.component';
+import { CareersComponent } from './views/careers/careers.component';
 
 export const routes: Routes = [
   { path: '', title:"Home Page", component: HomeViewComponent },
   { path: 'services', title:"Services Page", component: ServicesViewComponent },
   { path: 'training', title: "Training Page", component: TrainingViewComponent },
   { path: 'recruitment', title: "Recruitment Page", component: RecruitmentComponent },
-  { path: 'about-us', title: "About us Page", component: AboutUsComponent }
+  { path: 'about-us', title: "About us Page", component: AboutUsComponent },
+  { path: 'careers', title: "Careers Page", component: CareersComponent }
 ];
