dc9fc3aec1cf5b2635e6f6b178e4e7a98f737a13
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-22 20:24:55 -0400

    HC: add training section in training view

--- src/app/views/services-view/services-view.component.ts
+++ src/app/views/services-view/services-view.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { InfoCardComponent, articleI } from '../../components/info-card/info-card.component';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
 import { SafePipe } from '../../utils/pipes/safe.pipe';
+import { trainingData } from '../../utils/content/training';
 
 @Component({
   selector: 'app-services-view',
@@ -11,30 +12,8 @@ import { SafePipe } from '../../utils/pipes/safe.pipe';
   styleUrl: './services-view.component.css'
 })
 export class ServicesViewComponent {
-  trainingData: articleI[] = [
-    {
-      id: 1,
-      title: 'Presentación',
-      content: 'Bienvenido a nuestra sección de Formación, donde la excelencia y el desarrollo de habilidades convergen para impulsar el éxito de tu equipo y empresa...',
-      img: 'image-info-card-001.png',
-      url: ''
-    },
-    {
-      id: 2,
-      title: 'Especialización',
-      content: 'Desde liderazgo hastahabilidades técnicas especializadas, nuestros programas de formación estándiseñados para adaptarse a las necesidades específicas...',
-      img: 'image-info-card-002.png',
-      url: ''
-    },
-    {
-      id: 3,
-      title: 'Casos de Éxito',
-      content: 'Nuestros casos deéxito son testimonios tangibles de cómo la formación personalizada hatransformado empresas como la tuya...',
-      img: 'image-info-card-003.png',
-      url: ''
-    }
-  ]
-
+  trainingData = trainingData;
+  
   recruitmentData: articleI[] = [
     {
       id: 1,
--- src/app/views/training-view/training-view.component.ts
+++ src/app/views/training-view/training-view.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { trainingData } from '../../utils/content/training';
+import { InfoCardComponent } from '../../components/info-card/info-card.component';
 
 @Component({
   selector: 'app-training-view',
   standalone: true,
-  imports: [],
+  imports: [InfoCardComponent],
   templateUrl: './training-view.component.html',
   styleUrl: './training-view.component.css'
 })
 export class TrainingViewComponent {
+  trainingData = trainingData;
 
 }
