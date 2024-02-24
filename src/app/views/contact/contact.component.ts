22dbda3db0214f493bee2bee1255992e320bf86d
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-23 20:45:35 -0400

    EC: finish contact page

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { reviewsVideosData } from '../../utils/content/reviews-videos';
+import { SafePipe } from '../../utils/pipes/safe.pipe';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [SafePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  reviewsVideosData = reviewsVideosData;
+  videosLimit: number = 4;
 }
