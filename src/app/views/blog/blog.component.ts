955cd1b9d7db7e10d72342f59324a1c371302bdd
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-25 16:18:59 -0400

    HC: finish blog page

--- src/app/views/blog/blog.component.ts
+++ src/app/views/blog/blog.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, CarouselModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      992: {
+        items: 4
+      },
+    },
+    nav: true
+  }
 }
