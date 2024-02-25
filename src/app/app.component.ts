0caf704ab7020482ca185a3bf4aad0fa367bb535
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-25 17:49:07 -0400

    HC: add home animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavHeaderComponent } from './components/nav-header/nav-header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import * as AOS from 'aos'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,4 +12,9 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
   title = 'hercules-consulting-corp';
+
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load', AOS.refresh)
+  }
 }
