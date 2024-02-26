0c3dfabc170953d861e4f7e40ab99879fa0d175d
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-25 20:25:17 -0400

    HC: fix views animations bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavHeaderComponent } from './components/nav-header/nav-header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import * as AOS from 'aos'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavHeaderComponent, FooterComponent],
+  imports: [RouterOutlet, NavHeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hercules-consulting-corp';
-  constructor(private router: Router) {}
-  
-  ngOnInit() {
-    AOS.init()
-    this.router.events.subscribe((event) => {
-      window.addEventListener('load', AOS.refresh)
-    });
+  constructor() {}
+
+  ngAfterViewInit() {
+    document.onreadystatechange = function () {
+      if (document.readyState == "complete") {
+        AOS.init();
+        window.addEventListener('scroll', AOS.refresh);
+      } 
+    }
   }
 }
