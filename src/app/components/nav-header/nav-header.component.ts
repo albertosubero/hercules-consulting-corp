61ec7684d61f08dc06f023e73a43efe0126c829f
avsg1991@gmail.com <avsg1991@gmail.com>
2024-02-28 11:26:43 -0400

    HC: fix mobile styles bugs

--- src/app/components/nav-header/nav-header.component.ts
+++ src/app/components/nav-header/nav-header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { NavigationStart, Router, RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -12,9 +12,40 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 export class NavHeaderComponent {
   mobileMenuIsOpen: boolean = false;
   currentLang: string = '';
+  
+  public innerWidth: number = 0;
+  isResponsive: boolean = false;
 
-  constructor(public translate: TranslateService){
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 992) {
+      this.isResponsive = true;
+    } else {
+      this.isResponsive = false;
+    }
+  }
+
+  constructor(public translate: TranslateService, private router: Router){
     this.currentLang = this.translate.currentLang;
+  }
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 992) {
+      this.isResponsive = true;
+    } else {
+      this.isResponsive = false;
+    }
+    
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        // Navigation is starting...
+        if (this.isResponsive) {
+          this.mobileMenuIsOpen = false;
+        }
+      }
+    });
   }
 
   setTransLanguage(lang: string){
